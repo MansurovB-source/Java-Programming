4465097ac6a2674b3c1916dc6dcc251ee3f5331d
Behruz Mansurov <bekha.m2000.bm@gmail.com>
2020-08-11 03:19:50 +0300

    Added all parser methods for user input

--- Programming on Java Lab №5/src/Parser/UserInputParser.java
+++ Programming on Java Lab №5/src/Parser/UserInputParser.java
@@ -1,9 +1,7 @@
 package Parser;
 
-import Data.Address;
-import Data.Coordinates;
-import Data.Organization;
-import Data.OrganizationType;
+import Data.*;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -20,7 +18,6 @@ public class UserInputParser {
     private String read;
     private boolean check = false;
     private long cnt;
-    ZonedDateTime zonedDateTime;
     private ZoneId timeZone = ZoneId.systemDefault();
 
     public String inputName() {
@@ -34,7 +31,7 @@ public class UserInputParser {
 //                    in.nextLine();
                 } else
                     System.out.println("Wrong value please repeat!");
-                System.out.println();
+                    System.out.println();
             } else {
                 System.out.println("Wrong value please repeat!");
                 System.out.println();
@@ -45,7 +42,201 @@ public class UserInputParser {
         return read;
     }
 
-    public int inputCoordinatesX() {
+    public Coordinates inputCoordinates() {
+        System.out.println("Координата может быть null-ом\n" +
+                "Если хотите чтобы координата была null-ом\n" +
+                "Введите -1");
+        while (!check) {
+            if (in.hasNextInt()) {
+                cnt = in.nextInt();
+                if (cnt == -1) {
+                    break;
+                } else {
+                    return new Coordinates(inputCoordinatesX(), inputCoordinatesY());
+                }
+            }
+        }
+        return null;
+    }
+
+    public Double inputSalary() {
+        Double salary = null;
+        while (!check) {
+            System.out.println("Введите зарплату: ");
+            System.out.println("-1 это null\n" +
+                    "Значение поля должно быть больше 0");
+            if (in.hasNextDouble()) {
+                salary = in.nextDouble();
+                if (salary == -1) {
+                    salary = null;
+                    check = true;
+                    System.out.println();
+                    in.nextLine();
+                } else if (salary > 0) {
+                    check = true;
+                    System.out.println();
+                    in.nextLine();
+                } else
+                    System.out.println("Wrong value please repeat!");
+                System.out.println();
+            } else {
+                System.out.println("Wrong value please repeat!");
+                System.out.println();
+                in.next();
+                check = false;
+            }
+        }
+        check = false;
+        return salary;
+    }
+
+    public ZonedDateTime inputStartDate() {
+        ZonedDateTime zonedDateTime;
+        System.out.println("Введите дату поступления в формате YYYY-MM-DD HH:MM:SS");
+        read = in.nextLine();
+        zonedDateTime = LocalDateTime.parse(read,
+                DateTimeFormatter.ISO_DATE_TIME).atZone(timeZone);
+        return zonedDateTime;
+    }
+
+    public LocalDateTime inputEndDate() {
+        LocalDateTime localDateTime;
+        System.out.println("Введите дату окончания в формате YYYY-MM-DD HH:MM:SS");
+        System.out.println("Дата окончания может быть null-ом\n" +
+                " -1 это null");
+        read = in.nextLine();
+        if (read.equals("-1")) {
+            localDateTime = null;
+        } else {
+            localDateTime = LocalDateTime.parse(read,
+                    DateTimeFormatter.ISO_DATE_TIME);
+        }
+        return localDateTime;
+    }
+
+    public Status inputStatus() {
+        System.out.println("Статус:\n" +
+                "    FIRED,\n" +
+                "    HIRED,\n" +
+                "    RECOMMENDED_FOR_PROMOTION,\n" +
+                "    REGULAR,\n" +
+                "    PROBATION;" +
+                "");
+        while (!check) {
+            System.out.print("Введит Статус: ");
+            if (in.hasNextLine()) {
+                read = in.nextLine();
+                if (read.equals(Status.FIRED.toString()) || read.equals(Status.HIRED.toString())
+                        || read.equals(Status.RECOMMENDED_FOR_PROMOTION.toString())
+                        || read.equals(Status.REGULAR.toString()) || read.equals(Status.PROBATION.toString())) {
+                    check = true;
+                    System.out.println();
+                } else
+                    System.out.println("Wrong value please repeat!");
+                    System.out.println();
+            } else {
+                System.out.println("Wrong value please repeat! ");
+                System.out.println();
+                in.next();
+            }
+        }
+        check = false;
+        return Status.valueOf(read);
+    }
+
+    public Organization inputOrganization() {
+        System.out.println("Организация может быть null-ом\n" +
+                "Если хотите чтобы организация была null-ом\n" +
+                "Введите -1");
+        while (!check) {
+            if (in.hasNextInt()) {
+                cnt = in.nextInt();
+                if (cnt == -1) {
+                    break;
+                } else {
+                    return new Organization(inputOrganizationEmployeesCount(), inputOrganizationType(), inputAddress());
+                }
+            }
+        }
+        return null;
+    }
+
+    public Address inputAddress() {
+        System.out.println("Адресс может быть null-ом\n" +
+                "Если хотите чтобы адресс был null-ом\n" +
+                "Введите -1");
+        while (!check) {
+            if (in.hasNextInt()) {
+                cnt = in.nextInt();
+                if (cnt == -1) {
+                    break;
+                } else {
+                    return new Address(inputAddressStreet(), inputAddressZipCode());
+                }
+            }
+        }
+        return null;
+    }
+
+
+    //////////////////// Organization Methods ////////////////////
+    private long inputOrganizationEmployeesCount() {
+        while (!check) {
+            System.out.println("Введите количество сотрудников: ");
+            System.out.println("Введите целое число!\n" +
+                    "Значение поля должно быть больше 0");
+            if (in.hasNextLong()) {
+                cnt = in.nextLong();
+                if (cnt > 0) {
+                    check = true;
+                    System.out.println();
+                    in.nextLine();
+                } else
+                    System.out.println("Wrong value please repeat!");
+                System.out.println();
+            } else {
+                System.out.println("Wrong value please repeat!");
+                System.out.println();
+                in.next();
+                check = false;
+            }
+        }
+        check = false;
+        return cnt;
+    }
+
+    private OrganizationType inputOrganizationType() {
+        System.out.println("Тип организации:\n" +
+                "    COMMERCIAL,\n" +
+                "    TRUST,\n" +
+                "    PRIVATE_LIMITED_COMPANY,\n" +
+                "    OPEN_JOINT_STOCK_COMPANY;" +
+                "");
+        while (!check) {
+            System.out.print("Введите тип организации: ");
+            if (in.hasNextLine()) {
+                read = in.nextLine();
+                if (read.equals(OrganizationType.COMMERCIAL.toString()) || read.equals(OrganizationType.PRIVATE_LIMITED_COMPANY.toString())
+                        || read.equals(OrganizationType.PRIVATE_LIMITED_COMPANY.toString())
+                        || read.equals(OrganizationType.TRUST.toString())) {
+                    check = true;
+                    System.out.println();
+                } else
+                    System.out.println("Wrong value please repeat!");
+                System.out.println();
+            } else {
+                System.out.println("Wrong value please repeat! ");
+                System.out.println();
+                in.next();
+            }
+        }
+        check = false;
+        return OrganizationType.valueOf(read);
+    }
+
+
+    //////////////////// Coordinates Methods ////////////////////
+    private int inputCoordinatesX() {
         System.out.println("Coordinates { \n" +
                 "   x: \n" +
                 "   y: \n" +
@@ -73,7 +264,7 @@ public class UserInputParser {
         return (int) cnt;
     }
 
-    public long inputCoordinatesY() {
+    private long inputCoordinatesY() {
         while (!check) {
             System.out.println("Введите y: ");
             System.out.println("Введите целое число!\n" +
@@ -99,113 +290,15 @@ public class UserInputParser {
         return cnt;
     }
 
-    public Double inputSalary() {
-        Double salary = null;
-        while (!check) {
-            System.out.println("Введите зарплату: ");
-            System.out.println("-1 это null\n" +
-                    "Значение поля должно быть больше 0");
-            if (in.hasNextDouble()) {
-                salary = in.nextDouble();
-                if(salary == -1) {
-                    salary = null;
-                    check = true;
-                } else if(salary > 0) {
-                    check = true;
-                    System.out.println();
-//                    in.nextLine();
-                } else
-                    System.out.println("Wrong value please repeat!");
-                System.out.println();
-            } else {
-                System.out.println("Wrong value please repeat!");
-                System.out.println();
-                in.next();
-                check = false;
-            }
-        }
-        check = false;
-        return salary;
-    }
 
-    public ZonedDateTime inputStartDate() {
-        System.out.println("Введите дату поступления в формате YYYY-MM-DD HH:MM:SS");
-        read = in.nextLine();
-        zonedDateTime = LocalDateTime.parse(read,
-                DateTimeFormatter.ISO_DATE_TIME).atZone(timeZone);
-        return zonedDateTime;
-    }
-
-    public ZonedDateTime inputEndDate() {
-        System.out.println("Введите дату окончания в формате YYYY-MM-DD HH:MM:SS");
-        read = in.nextLine();
-        if(read.equals("-1")) {
-            zonedDateTime = null;
-        }
-        zonedDateTime = LocalDateTime.parse(read,
-                DateTimeFormatter.ISO_DATE_TIME).atZone(timeZone);
-        return zonedDateTime;
-    }
-
-    public long inputOrganizationEmployeesCount() {
-        while (!check) {
-            System.out.println("Введите количество сотрудников: ");
-            System.out.println("Введите целое число!\n" +
-                    "Значение поля должно быть больше 0");
-            if (in.hasNextLong()) {
-                cnt = in.nextLong();
-                if (cnt > 0) {
-                    check = true;
-                    System.out.println();
-                    in.nextLine();
-                } else
-                    System.out.println("Wrong value please repeat!");
-                System.out.println();
-            } else {
-                System.out.println("Wrong value please repeat!");
-                System.out.println();
-                in.next();
-                check = false;
-            }
-        }
-        check = false;
-        return cnt;
-    }
-
-    public OrganizationType inputOrganizationType() {
-        System.out.println("Тип организации:\n" +
-                "    COMMERCIAL,\n" +
-                "    TRUST,\n" +
-                "    PRIVATE_LIMITED_COMPANY,\n" +
-                "    OPEN_JOINT_STOCK_COMPANY;");
-        while (!check) {
-            System.out.print("Введите тип организации: ");
-            if (in.hasNextLine()) {
-                read = in.nextLine();
-                if (read.equals(OrganizationType.COMMERCIAL.toString()) || read.equals(OrganizationType.PRIVATE_LIMITED_COMPANY.toString())
-                        || read.equals(OrganizationType.PRIVATE_LIMITED_COMPANY.toString())
-                        || read.equals(OrganizationType.TRUST.toString())) {
-                    check = true;
-                    System.out.println();
-                } else
-                    System.out.println("Wrong value please repeat!");
-                System.out.println();
-            } else {
-                System.out.println("Wrong value please repeat! ");
-                System.out.println();
-                in.next();
-            }
-        }
-        check = false;
-        return OrganizationType.valueOf(read);
-    }
-
+    //////////////////// Address Methods ////////////////////
     private String inputAddressStreet() {
+        in.nextLine();
         while (!check) {
             System.out.print("Введите адрес: ");
             if (in.hasNextLine()) {
                 read = in.nextLine();
-                if(read.equals("-1")) {
+                if (read.equals("-1")) {
                     read = null;
                     check = true;
                 } else if (read != null) {
@@ -214,7 +307,7 @@ public class UserInputParser {
 //                    in.nextLine();
                 } else
                     System.out.println("Wrong value please repeat!");
-                    System.out.println();
+                System.out.println();
             } else {
                 System.out.println("Wrong value please repeat! ");
                 System.out.println();
@@ -235,7 +328,7 @@ public class UserInputParser {
                     System.out.println();
                 } else
                     System.out.println("Wrong value please repeat!");
-                    System.out.println();
+                System.out.println();
             } else {
                 System.out.println("Wrong value please repeat! ");
                 System.out.println();
@@ -246,37 +339,5 @@ public class UserInputParser {
         return read;
     }
 
-    public Address inputAddress() {
-        System.out.println("Адресс может быть null\n" +
-                "Если хотите чтобы адресс был null-ом\n" +
-                "Введите -1");
-        while(!check) {
-            if(in.hasNextInt()) {
-                cnt = in.nextInt();
-                if(cnt == -1) {
-                    break;
-                } else {
-                    return new Address(inputAddressStreet(), inputAddressZipCode());
-                }
-            }
-        }
-        return null;
-    }
 
-    public Organization inputOrganization() {
-        System.out.println("Организация может быть null\n" +
-                "Если хотите чтобы организация была null-ом\n" +
-                "Введите -1");
-        while(!check) {
-            if(in.hasNextInt()) {
-                cnt = in.nextInt();
-                if(cnt == -1) {
-                    break;
-                } else {
-                    return new Organization(inputOrganizationEmployeesCount(),inputOrganizationType(), inputAddress());
-                }
-            }
-        }
-        return null;
-    }
 }
